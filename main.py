28a5ef495c7c1f638c75eeeef5ea24f368db4855
Ilya Arkhanhelsky <dm@dm>
2013-08-18 15:35:55 +0300

    Placing objects on the grid

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pygame
 import world
 import visual
 
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1280, 840))
 
 
 screen.fill((240, 240, 240))
-b = world.new_object((2, 8), (2, 8), (20, 50))
+b = world.place(world.new_object((2, 8), (2, 8), (20, 50)), (5, 15))
 visual.draw_object(screen, b)
 
 pygame.display.flip()
--- world.py
+++ world.py
@@ -3,3 +3,6 @@ from random import randint
 
 def new_object(rows, cols, height): return ((0, 0 , randint(*rows), randint(*cols)), randint(*height), randclr())
 
+def place(object, at):
+	((i, j, rows, cols), height, color) = object
+	return (at + (rows, cols), height, color)

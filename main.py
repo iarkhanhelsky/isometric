be3427be5f4fe62c8eb0e3edd7f5f6010b327f58
Ilya Arkhanhelsky <dm@dm>
2013-08-21 23:07:37 +0300

    Parametric populate. Drawing world

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from functools import cmp_to_key
 screen = pygame.display.set_mode((1280, 840))
 
 screen.fill((240, 240, 240))
-b = world.place(world.new_object((2, 8), (2, 8), (20, 50)), (0, 0))
-for obj in sorted(world.populate(500), key=cmp_to_key(world.cmp_objects)):
+
+for obj in sorted(world.populate(500, 200, 200), key=cmp_to_key(world.cmp_objects)):
 	visual.draw_object(screen, obj)
 
 pygame.display.flip()

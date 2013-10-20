044da0f1932831a2abd2751e0722dbd073427fa1
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2013-10-20 15:25:27 +0300

    main.py refactoring. We all love oneliners

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from pygame.constants import KEYDOWN, K_ESCAPE, K_DOWN, QUIT, K_RIGHT, K_LEFT, K
 from pygame import Surface
 import world
 import visual
+from visual import vecadd
 from functools import cmp_to_key
-from utils import flatten
 
-ROWS = 300
-COLS = 300
-BUILDINGS = 100
+(ROWS, COLS, BUILDINGS) = (300, 300, 100)
 
 
 def draw(view, world_objects):
@@ -20,10 +18,12 @@ def cmp_objects(a, b):
     return world.cmp_objects(a, b, ROWS * COLS)
 
 
-roads = [(x, 0, 0xCCCCCC) for x in
-         flatten(world.road_populate([[0 for _ in range(COLS)] for _ in range(ROWS)], 10, 10, 0, 0, ROWS, COLS))]
-buildings = world.populate(BUILDINGS, ROWS, COLS)
-objects = sorted(roads + buildings, key=cmp_to_key(cmp_objects))
+objects = sorted(
+    [
+        ((120, 0, 100, 100,  0), 96, 0x404040),
+        ((120, 0, 100, 100, 96),  0, 0x10FF10),
+        ((60,  0, 300, 300,  0),  0, 0x10FF10)
+    ], key=cmp_to_key(cmp_objects))
 
 surf = Surface((10000, 10000))
 
@@ -33,31 +33,29 @@ draw(surf, objects)
 running = True
 redraw = True
 
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((1600, 900))
 screen.fill((240, 240, 240))
 move = (-1390, -40)
+
+#noinspection PyArgumentList
+pygame.key.set_repeat(500, 30)
+
+move_dict = {
+    K_DOWN: (0, -10),
+    K_UP: (0, 10),
+    K_LEFT: (10, 0),
+    K_RIGHT: (-10, 0)
+}
 
 while running:
     events = pygame.event.get()
     for e in events:
-        if e.type == KEYDOWN:
+        running = e.type != QUIT and not (e.type == KEYDOWN and e.key == K_ESCAPE)
+        if e.type == KEYDOWN and e.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
             redraw = True
-
-            if e.key == K_ESCAPE:
-                running = False
-            elif e.key == K_DOWN:
-                move = (move[0], move[1] - 10)
-            elif e.key == K_UP:
-                move = (move[0], move[1] + 10)
-            elif e.key == K_LEFT:
-                move = (move[0] - 10, move[1])
-            elif e.key == K_RIGHT:
-                move = (move[0] + 10, move[1])
-        elif e.type == QUIT:
-            running = False
+            move = vecadd(move, move_dict[e.key])
 
     if redraw:
-        print(move)
         redraw = False
         screen.fill((240, 240, 240))
         screen.blit(surf, move)
